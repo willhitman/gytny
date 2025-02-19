70fd84849eba9739fa917ae18c5eb93139f164a8
giftw <giftwt9wt@gmail.com>
2025-02-19 02:09:08 +0200

    ...

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-tuhp2x+udb0vm%2lx@vi5q_8kl@avb0j1qx714ce7%1ah5@=12
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['18.135.25.233','localhost', '127.0.0.1:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['18.135.25.233', 'localhost', '127.0.0.1:8000', '127.0.0.1']
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
@@ -166,9 +166,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
 }
 
 SIMPLE_JWT = {
