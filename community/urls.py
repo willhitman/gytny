6fc87ddd18ded507d3fcb72717f5d46b26d0a94a
giftw <giftwt9wt@gmail.com>
2025-02-27 15:58:51 +0200

    adding get chat rooms route

--- community/urls.py
+++ community/urls.py
@@ -1,8 +1,14 @@
 # community/urls.py
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from .views import WebSocketInfoView, CreateChatRoomView
+
+from .async_messaging.routing import websocket_urlpatterns
+from .views import WebSocketInfoView, CreateChatRoomView, GetChatRoomsView
 
 urlpatterns = [
     path('api/websocket-info/', WebSocketInfoView.as_view(), name='websocket-info'),
     path('api/create-room/', CreateChatRoomView.as_view(), name='create-room'),
+    path('api/get-rooms/', GetChatRoomsView.as_view(), name='get-rooms'),
 ]
+
