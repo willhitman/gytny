700468ccd5d3de768573980888e6a0871e6cdbfe
giftw <giftwt9wt@gmail.com>
2025-03-03 04:34:06 +0200

    ...

--- core/asgi.py
+++ core/asgi.py
@@ -1,5 +1,7 @@
 # myproject/asgi.py
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -8,7 +10,7 @@ from community.async_messaging.routing import websocket_urlpatterns
 from core.middleware import WebSocketJWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
+django.setup()
 ''' django socket not preconfigured to work with jwt late alone reading headers so we need to create a 
 middleware to do that '''
 application = ProtocolTypeRouter({
