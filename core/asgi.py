64a82e4bb4c84449118f3a2ac52706fead136e9a
giftw <giftwt9wt@gmail.com>
2025-03-06 20:09:58 +0200

    ...

--- core/asgi.py
+++ core/asgi.py
@@ -1,4 +1,6 @@
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -6,6 +8,7 @@ from community.async_messaging.routing import websocket_urlpatterns
 from core.middleware import WebSocketJWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
