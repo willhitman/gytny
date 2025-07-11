a36eeead102a16edf249bfa919312f12c66ccc4b
willhitman <giftwt9wt@gmail.com>
2025-07-11 22:44:20 +0200

    changed community socket router to use chat_id instead of model primary key for added ambiguity and consequently.... security

--- community/async_messaging/routing.py
+++ community/async_messaging/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from community.async_messaging.consumers import CommunityChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/chat/(?P<room_id>\d+)/$', CommunityChatConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<chat_id>[0-9A-F]{16})/$', CommunityChatConsumer.as_asgi()),
 ]
