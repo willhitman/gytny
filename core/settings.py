2b2d2d94d864f1e42381ce042a0f86c0e9744f99
willhitman <giftwt9wt@gmail.com>
2025-08-05 21:21:33 +0200

    removing default throttling classes

--- core/settings.py
+++ core/settings.py
@@ -174,16 +174,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '2/hour',
-        'user': '1000/day'
-    }
 }
 
 
